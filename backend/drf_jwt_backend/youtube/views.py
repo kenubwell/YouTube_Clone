81fb47fc338ad4a59ff6a64da9570227a1c2451c
unknown <kenblackuoo@gmail.com>
2022-03-16 17:25:54 -0400

    Validated CommentsList Class functions + Valdiated the Get Function within CommentDetail Class

--- backend/drf_jwt_backend/youtube/views.py
+++ backend/drf_jwt_backend/youtube/views.py
@@ -2,26 +2,17 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import permission_classes
 from rest_framework.views import APIView
 from .models import Comment, Reply
 from .serializers import CommentSerializer, ReplySerializer 
 
 
 
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def get_all_cars(request):
-    comments = Comment.objects.all()
-    serializer = CommentSerializer(comments, many=True)
-    return Response(serializer.data)
-
-
-
-
 class CommentsList(APIView):
     # A class-based view that lists all songs or creates a new song
-    def get(self, request, format=None):
+    @permission_classes([AllowAny])
+    def get(self, request):
         comments = Comment.objects.all()
         # the following serializer is going to take our songs table and convert to json
         serializers = CommentSerializer(comments, many=True)
@@ -29,37 +20,39 @@ class CommentsList(APIView):
 
 
     @permission_classes([IsAuthenticated])
-    def post(self, request, format=None):
+    def post(self, request):
         serializers = CommentSerializer(data=request.data)
         # the following validates that API user input is true or accurate to the database
         serializers.is_valid(raise_exception=True)
-        serializers.save()
+        serializers.save(user=request.user)
         return Response(serializers.data, status=status.HTTP_201_CREATED)
+
 
 class CommentDetail(APIView):
     # A class-based view that retrieves (by id), updates, or deletes a song
 
     @permission_classes([IsAuthenticated])
-    def get(self, request, pk, format=None):
-        comment = get_object_or_404(Comment, pk=pk)
+    def get(self, request, pk):
+        comment = get_object_or_404(Comment.objects.filter(user_id=request.user.id), pk=pk)
         serializer = CommentSerializer(comment)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-#     @permission_classes([IsAuthenticated])
-#     def put(self, request, pk, format=None):
-#         comment = get_object_or_404(Comment, pk=pk)
-#         serializer = CommentSerializer(comment, data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save(user=request.user)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
 
-#     @permission_classes([IsAuthenticated])
-#     def post(self, request, format=None):
-#         serializers = CommentSerializer(data=request.data)
-#         # the following validates that API user input is true or accurate to the database
-#         serializers.is_valid(raise_exception=True)
-#         serializers.save(user=request.user)
-#         return Response(serializers.data, status=status.HTTP_201_CREATED)
+    @permission_classes([IsAuthenticated])
+    def put(self, request, pk, format=None):
+        comment = get_object_or_404(Comment, pk=pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @permission_classes([IsAuthenticated])
+    def post(self, request, format=None):
+        serializers = CommentSerializer(data=request.data)
+        # the following validates that API user input is true or accurate to the database
+        serializers.is_valid(raise_exception=True)
+        serializers.save(user=request.user)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
 
 class ReplyList(APIView):
     # A class-based view that lists all songs or creates a new song
