0adb1c023a81dea7d9d19238e4c5a2c7871e526a
unknown <kenblackuoo@gmail.com>
2022-03-17 17:50:28 -0400

    Updated and validated authentication per each class function

--- backend/drf_jwt_backend/youtube/views.py
+++ backend/drf_jwt_backend/youtube/views.py
@@ -8,18 +8,20 @@ from .models import Comment, Reply
 from .serializers import CommentSerializer, ReplySerializer 
 
 
+
+@permission_classes([AllowAny])
 class VideoComments(APIView):
 
-    @permission_classes([AllowAny])
     def get(self, request, video_id):
         video_comments = Comment.objects.filter(video_id = video_id)
         serializers = CommentSerializer(video_comments, many=True)
         return Response(serializers.data, status=status.HTTP_200_OK)
 
 
+
+@permission_classes([IsAuthenticated])
 class CommentPost(APIView):
 
-    @permission_classes([IsAuthenticated])
     def post(self, request):
         serializers = CommentSerializer(data=request.data)
         # the following validates that API user input is true or accurate to the database
@@ -28,9 +30,10 @@ class CommentPost(APIView):
         return Response(serializers.data, status=status.HTTP_201_CREATED)
 
 
+
+@permission_classes([IsAuthenticated])
 class CommentUpdate(APIView):
-    
-    @permission_classes([IsAuthenticated])
+     
     def put(self, request, pk):
         comment = get_object_or_404(Comment, pk=pk)
         serializer = CommentSerializer(comment, data=request.data)
@@ -39,16 +42,16 @@ class CommentUpdate(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+
+@permission_classes([IsAuthenticated])
 class ReplyDetails(APIView):
 
-    @permission_classes([IsAuthenticated])
     def get(self, request, pk):
         replies = Reply.objects.filter(comment_id = pk)
         serializers = ReplySerializer(replies, many=True)
         return Response(serializers.data, status=status.HTTP_200_OK)
 
 
-    @permission_classes([IsAuthenticated])
     def post(self, request, pk):
         comment_id = pk
         temp_data = request.data
