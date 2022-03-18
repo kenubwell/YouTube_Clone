b1871cb319c0523571051542c7e0e448af21993b
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-03-18 15:08:41 -0600

    Adjusted search page to not have double NavBar on correcting Url

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -3,13 +3,13 @@ import React, { useContext, useEffect } from "react";
 // import useCustomForm from "../../hooks/useCustomForm";
 // import { Link } from "react-router-dom";
 import "./SearchPage.css";
-import Navbar from "../../components/NavBar/NavBar";
+
 
 const SearchPage = () => {
 
   return (
     <div>
-       <Navbar/> 
+       
     </div>
   );
 };
