24b4255aebbd83a5b11081ef69b4acff331da8ba
unknown <kenblackuoo@gmail.com>
2022-03-18 16:49:01 -0400

    Added some logic (code) to the SearchBar component

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (  
-        <div></div>
+    <div>
+        <label>Search:</label>
+        <input type='text' placeholder="Search for a video" onChange={(event) => setSearchTerm(event.target.value)}/>
+    </div>
     );
 }
  
