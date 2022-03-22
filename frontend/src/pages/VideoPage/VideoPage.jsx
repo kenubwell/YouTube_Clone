5e8b9595a683d4b4d216351cd451ca2fa655b17c
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-03-22 13:51:57 -0600

    Add 6 videos to searchpage + Created working searchbar for the page + Need to do more research on how to display 6 videos independently instead of repeating the same video 6 times after a search maybe research in grabbing on element from an array + Require more code

--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {useEffect, useState } from "react";
 // import AuthContext from "../../context/AuthContext";
 // import useCustomForm from "../../hooks/useCustomForm";
 import { Link } from "react-router-dom";
@@ -8,32 +8,52 @@ import axios from 'axios'
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 
 const VideoPage = () => {
+  
+  const [searchResults, setSearchResults] = useState([]);
+  const [videoId, setVideoId] = useState([""]);
+  
+
+  useEffect(() => {
+    getSearchResults();
+  },[videoId])
 
 
+  let key = process.env.REACT_APP_API_KEY
+
+  async function getSearchResults(searchTerm='programming'){
+  let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&maxResults=6&key=AIzaSyDVQlyvw2k-xYN3K078IKgdz39tzDlmmhQ`);
+  setVideoId(response.data.items[0,1,2,3,4,5].id.videoId)
+  setSearchResults(response.data.items)
+  console.log(response.data.items)
+  }
+
+
+
+  
 
   return (
     <div>
       <div className="video-reg-contain"><Link to="/register" ><b>Register Here</b></Link></div>
-      <div><SearchBar/></div>
+      <div><SearchBar getSearchResults={getSearchResults}/></div>
       <div className = 'video-contain'>
         <div className="video-container">
             <div>
-              <VideoPlayer />
+              <VideoPlayer videoId={videoId[0]} />
             </div>
             <div>
-              <VideoPlayer/>
+              <VideoPlayer videoId={videoId[1]}/>
             </div>
             <div>
-              <img src ="/images/bmore3.jpg"/>
+              <VideoPlayer videoId={videoId[2]}/>
             </div>
             <div>
-              <img src ="/images/bmore4.jpg"/>
+              <VideoPlayer videoId={videoId[3]}/>
             </div>
             <div>
-              <img src ="/images/bmore5.jpg"/>
+              <VideoPlayer videoId={videoId[4]}/>
             </div>
             <div>
-              <img src ="/images/bmore6.jpg"/>
+              <VideoPlayer videoId={videoId[5]}/>
             </div>
         </div> 
       </div>
