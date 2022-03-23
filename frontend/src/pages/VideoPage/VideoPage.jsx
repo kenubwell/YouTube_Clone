d38219b3be48571504117ae342e21606d3d4c741
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-03-23 08:58:05 -0600

    Correct issue when searching for a singular item it would revert back to the default search value + Add Video ID/Video Description pulled from youtube API

--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -5,22 +5,27 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 import axios from 'axios'
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 
+let key = process.env.REACT_APP_API_KEY
+
 const VideoPage = () => {
   
-  const [searchResults, setSearchResults] = useState([]);
-  const [videoId, setVideoId] = useState([""]);
+  const [searchResults, setSearchResults] = useState('');
+  const [videoId, setVideoId] = useState("");
+  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
+
   
 
   useEffect(() => {
     getSearchResults();
-  },[videoId])
+  },[])
 
-
-  let key = process.env.REACT_APP_API_KEY
 
   async function getSearchResults(searchTerm='welcome to youtube'){
-  let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&maxResults=1&key=AIzaSyDVQlyvw2k-xYN3K078IKgdz39tzDlmmhQ`);
+  let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&maxResults=1&key=${key}`);
   setVideoId(response.data.items[0].id.videoId)
+  setDescription(response.data.items[0].snippet.description)
+  setTitle(response.data.items[0].snippet.title)
   setSearchResults(response.data.items)
   console.log(response.data.items)
 }
@@ -36,7 +41,7 @@ const VideoPage = () => {
       <div className = 'video-contain'>
         <div className="video-container">
             <div>
-              <VideoPlayer videoId={videoId} />
+              <div className='landing-page-video-player'><VideoPlayer videoId={videoId} description={description} title={title}/></div>
             </div>
         </div> 
       </div>
