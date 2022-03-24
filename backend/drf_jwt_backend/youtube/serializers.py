8c9b6f17209e5471e0da04ea0c43ff0f3e6302bb
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-03-24 15:09:06 -0600

    Add User serializer in order to display the username of the user trying to comment

--- backend/drf_jwt_backend/youtube/serializers.py
+++ backend/drf_jwt_backend/youtube/serializers.py
@@ -1,11 +1,19 @@
+from dataclasses import field
 from rest_framework import serializers
 from .models import Comment, Reply
+from django.contrib.auth.models import User
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username"]
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many =False, read_only=True)
     class Meta:
         model = Comment
-        fields = ['id', 'video_id', 'text', 'likes', 'dislikes', 'user_id']
+        fields = ['id', 'video_id', 'text', 'likes', 'dislikes', 'user']
         depth = 1
 
 
